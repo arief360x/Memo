8c131f0db0075f549ca524b760ab7f4610646753
Arief D <arief360x@gmail.com>
2022-04-30 15:34:47 +0800

    add controler for memo

---
+++ NotesWeb/Controllers/MemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotesWeb.Controllers
+{
+    public class MemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
