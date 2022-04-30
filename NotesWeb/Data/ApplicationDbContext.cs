e6d7f3583894e95ece20f1cfe70f2de1276a352f
Arief D <arief360x@gmail.com>
2022-04-30 14:53:55 +0800

    first commit

---
+++ NotesWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesWeb.Models;
+
+namespace NotesWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<MemoModel> Memos { get; set; }
+    }
+}
---
+++ NotesWeb/Models/MemoModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotesWeb.Models
+{
+    public class MemoModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string MemoCategory { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string MemoContent { get; set; }
+        public DateTime MemoCreatedTime { get; set; } = DateTime.Now;
+
+    }
+}
