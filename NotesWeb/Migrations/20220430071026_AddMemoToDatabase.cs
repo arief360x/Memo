f665a97a0d1db60a383b10cd4dc77fbc9b12757f
Arief D <arief360x@gmail.com>
2022-04-30 15:12:20 +0800

    add migration memotodatabase

---
+++ NotesWeb/Migrations/20220430071026_AddMemoToDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotesWeb.Migrations
+{
+    public partial class AddMemoToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Memos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MemoCategory = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemoContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MemoCreatedTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Memos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Memos");
+        }
+    }
+}
